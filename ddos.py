d545f41565f4a38d1d178f0667f51eb8408999bf
alien-keric <alienkeric@gmail.com>
2024-04-08 10:07:36 -0400

    second commit..

--- ddos.py
+++ ddos.py
@@ -1,3 +1,18 @@
 #!/bin/env python3
+import requests
+
+target = input("Enter the target name or ip or domain name or url: ")
+
+
+##sending request to the server
+
+data = {'key1': 'value1', 'key2': 'value2'}
+
+##sending a post to the server
+response = requests.post(target, data=data)
+
+##printing the status code
+print(response.status_code)
+
 
 
